fd4401711b6dcdbac0ff2b5c10ce0b3536b4d0e6
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-01-28 22:04:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+TnBotz = Cilent(
+     name="Payment_TN_Bot", 
+     api_id=API_ID, 
+     api_hash=API_HASH, 
+     bot_token=BOT_TOKEN
+) 
+
+
+Print("Bot Was Started") 
+
+TnBotz.run
