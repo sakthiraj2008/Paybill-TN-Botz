2ac6c85aa84e88732c3cb7e946076792e1915168
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-02-15 07:42:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from pyrogram import Client, filters
-import re
-import os
-from os import environ
-from Script import script
 
 
 API_ID = "11472991"
 API_HASH = "c78c50d54baf2173e8b3f75c359c0c72"
 BOT_TOKEN = "7238940586:AAHnEBgiRloqtZyGd3IPAyPGS01d2g8Jr4A"
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS','7188069786').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Paybill_UPI_BOT") # without @
-PORT = environ.get("PORT", "8080")
-
-DB_URI = environ.get("DB_URI", "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 TnBotz = Cilent(
      name="PayBill_UPI_Bot", 
@@ -26,12 +16,28 @@ TnBotz = Cilent(
 
 @TNBotz.on_message(filters.command("start")) 
 async def start_cmd(client, message):
-     print("START Command"):
+     await meaasge.replay_photo(
+          photo="https://envs.sh/EyO.jpg",
+          caption="ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ {} 👋, ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ʙᴏᴛ ᴀɴᴅ ʙᴇꜱᴛ ᴜɪ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇ ")
 
 
-@TnBotz.on_message(filters.command("help")) 
-async def help_cmd(client, message):
-     print("Help Command") 
+@TnBotz.on_message(filters.command("about")) 
+async def about_cmd(client, message):
+     await message.replay(
+              ABOUT_TXT = """<b>ʜɪ ɪ ᴀᴍ ᴘᴇʀᴍᴀɴᴇɴᴛ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ ᴡɪᴛʜ ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ + ᴄᴜsᴛᴏᴍ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ɪᴛ ᴍᴇᴀɴs ᴀɴʏ ᴜsᴇʀ ᴄᴀɴ sᴇᴛ ʜɪs ᴜʀʟ sʜᴏʀᴛɴᴇʀ ᴀɴᴅ + ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ.
+
+🤖 ᴍʏ ɴᴀᴍᴇ: {}
+
+📝 ʟᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>𝕻𝖞𝖙𝖍𝖔𝖓3</a>
+
+📚 ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>𝕻𝖗𝖔𝖌𝖗𝖆𝖒</a>
+
+🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/itzz_me_alexxa>𝓐𝖑𝖊𝖝𝖝𝖆</a>
+
+👥 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ: <a href=https://t.me/+eBzYtdaY7Bc2ZDVl>𝕿𝕹 𝕾𝖚𝖕𝖕𝖔𝖗𝖙</a>
+
+📢 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/tn_botz>𝕿𝕹 𝖀𝖕𝖉𝖆𝖙𝖊𝖘</a></b>
+""")
 
 
 Print("Bot Was Started") 
