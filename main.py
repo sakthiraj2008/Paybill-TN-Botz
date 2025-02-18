cb77ef4f5963b63ab612d3376592e33f3321bb8e
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-02-17 16:22:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, filters
-
+from pyrogram.types import InlineKeybordMarkup, InlineKeybordButton
 
 API_ID = "11472991"
 API_HASH = "c78c50d54baf2173e8b3f75c359c0c72"
-BOT_TOKEN = "7238940586:AAHnEBgiRloqtZyGd3IPAyPGS01d2g8Jr4A"
-
+BOT_TOKEN = "7653029123:AAGZT26o00FphPZloCohvv-QI5-jFKIMBH4"
 
 TnBotz = Cilent(
      name="PayBill_UPI_Bot", 
@@ -14,14 +13,18 @@ TnBotz = Cilent(
 ) 
 
 
+START_BUTTONS = [[
+  InlineKeybordButtons("Update Channel",url="https://t.me/tn_botz")
+]]
+
+
+
 @TnBotz.on_message(filters.command("start")) 
 async def start_cmd(client, message):
-     await meaasge.replay_photo(
-          photo="https://envs.sh/EyO.jpg",
-          caption="ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ {} 👋, ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ʙᴏᴛ ᴀɴᴅ ʙᴇꜱᴛ ᴜɪ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇ ")
-
-
-
+     await meaasge.replay_text(
+          text=="you have to join my channel to use me",
+          replay_markup=InlineKeybordMarkup(START_BUTTONS)
+     )
 Print("Bot Was Started") 
 
 TnBotz.run() 
